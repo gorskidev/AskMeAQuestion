3c473c9cc55c1e3a02592ec1c405934aa5627645
gorskidev <maciej.gorski995@gmail.com>
2020-02-08 13:11:55 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import questionLoader from './questionimporter.js';
+import answerLoader from './answerimporter.js'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Merger extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      random: Math.floor(Math.random() * 9) + 1,
+      showAnswer: false,
+      clicks: 0
+    }
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+  }
+   /* this.setState({
+      clicks: this.state.clicks + 1
+    }, () => {
+      console.log("is", this.state.clicks)
+    });*/
+
+  forceUpdateHandler(){
+    this.setState({
+      //showAnswer: true,
+      clicks: 0
+    }, function(){
+      const question = document.getElementById("question");
+      const answer = document.getElementById("answer");
+      
+      //if(!this.state.showAnswer){
+        document.getElementById("answerButton").innerHTML = "Show me answer";
+        questionLoader(question, this.state.random);
+        answer.innerHTML = "";
+        this.setState(state =>({
+          showAnswer: true,
+        }));
+     // }
+      
+      if(this.state.showAnswer){
+        answerLoader(answer, this.state.random);
+        this.setState(state =>({
+          showAnswer: false,
+          random: Math.floor(Math.random() * 9) + 1
+        }))
+        document.getElementById("answerButton").innerHTML = "Ask me another question";
+      }
+      /*else{
+        answerLoader(answer, this.state.random);
+          this.setState(state =>({
+            random: Math.floor(Math.random() * 9) + 1
+          }))
+          document.getElementById("answerButton").innerHTML = "Ask me question";
+      }*/
+    });
+
+      //random: Math.floor(Math.random() * 3),
+  };
+
+  render(){
+    return(
+      <div>
+        <div id="question">
+        </div>
+        <div id="answer">
+        </div>
+        <button id="answerButton" onClick = {this.forceUpdateHandler}> Ask me a question </button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(){
+    super();
+  }
+  
+  render(){
+    return(
+      <div>
+        <Merger />
+      </div>
+    )
+  }
 }
 
 export default App;
